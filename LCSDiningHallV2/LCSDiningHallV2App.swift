1a9623c56c96594c1e4078cd169a17db994f57f5
Nathan Smith <nsmith@lcs.on.ca>
2022-11-03 16:42:24 -0400

    Initial Commit

---
+++ LCSDiningHallV2/LCSDiningHallV2App.swift
@@ -0,0 +1,17 @@
+//
+//  LCSDiningHallV2App.swift
+//  LCSDiningHallV2
+//
+//  Created by Nathan on 2022-11-03.
+//
+
+import SwiftUI
+
+@main
+struct LCSDiningHallV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
